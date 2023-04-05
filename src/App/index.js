5f5d92d1557eaeb116780551a20e83c6eb24fd60
Marco Alducin <sokram_r5_1992@hotmail.com>
2023-04-05 13:04:35 -0600

    Instalacion de TailwindCSS, ajuste de header, main y footer y reacomodo de carpeta App

---
+++ src/App/index.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+    <React.Fragment>
+      <header className='border'>header</header>
+      <main className='flex flex-1 border'>main</main>
+      <footer className='border'>footer</footer>
+    </React.Fragment>
+  );
+}
+
+export default App;
