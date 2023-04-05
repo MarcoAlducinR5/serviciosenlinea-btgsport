bd352da95f7d9e2b0a33d2c4a0643c93e0211560
Marco Alducin <sokram_r5_1992@hotmail.com>
2023-04-05 14:25:31 -0600

    Fondo de pantalla principal

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      '2xs': '390px',
+      'xs': '475px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+      '3xl': '1600px',
+    },
     extend: {},
   },
   plugins: [],
