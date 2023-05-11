cb5966ff7f97417e2c87905a3017b6ee8b00d832
Marco Alducin <sokram_r5_1992@hotmail.com>
2023-05-11 11:23:33 -0600

    commit respaldo

---
+++ src/Components/Main/Ingresa/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Ingresa(){
+    return(
+        <React.Fragment>
+            <div class="3xs1:basis-1/2 3xs2:basis-[31%] 3xs3:basis-[30%] 2xs1:basis-[28.5%] 2xs2:basis-[30%] 2xs3:basis-[31%] 2xs4:basis-[35.5%] border-2">
+                {/* 2xs5:basis-[49%] sm1:basis-[71.5%] sm2:basis-[56.5%] md1:basis-[55%] md2:basis-[54.5%] md3:basis-[55.5%] lg:basis-[64%] xl:basis-3/5 2xl:basis-3/5  */}
+            </div>
+            <div class="3xs1:basis-1/2 3xs2:basis-[69%] 3xs3:basis-[70%] 2xs1:basis-[71.5%] 2xs2:basis-[70%] 2xs3:basis-[69%] 2xs4:basis-[64.5%] text-center font-bold border-2">
+                {/* 2xs5:basis-[51%] sm1:basis-[28.5%] sm2:basis-[43.5%] md1:basis-[45%] md2:basis-[45.5%] md3:basis-[44.5%] lg:basis-[41%] xl:basis-2/5 2xl:basis-2/5  */}
+            </div>
+        </React.Fragment>
+    );
+}
+
+export {Ingresa};
--- src/Components/Main/index.js
+++ src/Components/Main/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Principal } from './Principal';
+import { Ingresa } from './Ingresa';
 
 function Main(){
 
     return(
         <div class="flex 3xs1:flex-col lg:flex-row w-full">
-            <Principal />
+            {/* <Principal /> */}
+            <Ingresa />
         </div>
     );
 }
